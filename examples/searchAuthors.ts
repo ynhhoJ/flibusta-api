a3f9c07274b819174d1ce92aa7a1b73bfe1000c0
ynhhoJ <lJohhnyl@outlook.com>
2022-02-14 23:53:36 +0200

    🚧 Add & Implement in examples new api usage

--- examples/searchAuthors.ts
+++ examples/searchAuthors.ts
@@ -1,13 +1,14 @@
-import { searchAuthors } from '../src';
+import FlibustaAPI from '../src/index';
 
 (async () => {
   // eslint-disable-next-line unicorn/no-unreadable-array-destructuring
-  const [/* original value */, /* path to the file */, author, itemsLimit, pageNumber] = process.argv;
+  const [/* original value */, /* path to the file */, authorName, pageNumber, itemsLimit] = process.argv;
+  const flibustaApi = new FlibustaAPI('http://flibusta.is/');
 
-  const itemsLimitAsInt = Number.parseInt(itemsLimit, 10);
+  const itemsLimitAsInt = Number.parseInt(itemsLimit ?? 50, 10);
   const pageNumberAsInt = Number.parseInt(pageNumber ?? 0, 10);
 
-  const searchBooksByNameResult = await searchAuthors(author, itemsLimitAsInt, pageNumberAsInt);
+  const searchBooksByNameResult = await flibustaApi.getAuthors(authorName, pageNumberAsInt, itemsLimitAsInt);
 
   console.log(JSON.stringify(searchBooksByNameResult, undefined, 2));
 })();
--- examples/searchBooksBySeries.ts
+++ examples/searchBooksBySeries.ts
@@ -1,13 +1,14 @@
-import { searchBooksBySeries } from '../src';
+import FlibustaAPI from '../src/index';
 
 (async () => {
   // eslint-disable-next-line unicorn/no-unreadable-array-destructuring
-  const [/* original value */, /* path to the file */, bookName, itemsLimit, pageNumber] = process.argv;
+  const [/* original value */, /* path to the file */, bookName, pageNumber, itemsLimit] = process.argv;
+  const flibustaApi = new FlibustaAPI('http://flibusta.is/');
 
-  const itemsLimitAsInt = Number.parseInt(itemsLimit, 10);
+  const itemsLimitAsInt = Number.parseInt(itemsLimit ?? 50, 10);
   const pageNumberAsInt = Number.parseInt(pageNumber ?? 0, 10);
 
-  const searchBooksByNameResult = await searchBooksBySeries(bookName, itemsLimitAsInt, pageNumberAsInt);
+  const searchBooksByNameResult = await flibustaApi.getBooksBySeries(bookName, pageNumberAsInt, itemsLimitAsInt);
 
   console.log(JSON.stringify(searchBooksByNameResult, undefined, 2));
 })();
