8af9e0495efca7575d5b875e59e19b79155ae9e0
ynhhoJ <lJohhnyl@outlook.com>
2022-02-22 17:56:24 +0200

    🚧 Examples: Add & Implement searchAuthorsOpds

---
+++ examples/searchAuthorsOpds.ts
@@ -0,0 +1,12 @@
+import FlibustaAPI from '@src/index';
+
+(async () => {
+  // eslint-disable-next-line unicorn/no-unreadable-array-destructuring
+  const [/* original value */, /* path to the file */, authorIdString] = process.argv;
+  const flibustaApi = new FlibustaAPI();
+  const authorId = Number.parseInt(authorIdString, 10);
+
+  const searchBooksByNameResult = await flibustaApi.getAuthorsOpds(authorId);
+
+  console.log(JSON.stringify(searchBooksByNameResult, undefined, 2));
+})();
---
+++ examples/searchAuthorsOpdsPaginated.ts
@@ -0,0 +1,14 @@
+import FlibustaAPI from '@src/index';
+
+(async () => {
+  // eslint-disable-next-line unicorn/no-unreadable-array-destructuring
+  const [/* original value */, /* path to the file */, authorIdString, pageNumber, itemsLimit] = process.argv;
+  const flibustaApi = new FlibustaAPI();
+  const authorId = Number.parseInt(authorIdString, 10);
+
+  const itemsLimitAsInt = Number.parseInt(itemsLimit ?? 50, 10);
+  const pageNumberAsInt = Number.parseInt(pageNumber ?? 0, 10);
+  const searchBooksByNameResult = await flibustaApi.getAuthorsOpdsPaginated(authorId, pageNumberAsInt, itemsLimitAsInt);
+
+  console.log(JSON.stringify(searchBooksByNameResult, undefined, 2));
+})();
