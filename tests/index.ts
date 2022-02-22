a34ad660737515c48ffc11aeaa94e71587d45c0b
ynhhoJ <lJohhnyl@outlook.com>
2022-02-22 18:46:52 +0200

    ✅ Add GetAuthorsOpds tests

--- tests/index.ts
+++ tests/index.ts
@@ -73,6 +73,110 @@ describe('FlibustaAPI', () => {
       });
     });
   });
+  
+  describe('GetAuthorsOpds', () => {
+    // NOTE: Arthur Conan Doyle
+    const authorId = 6116;
+    
+    describe('getAuthorsFromOpds()', async () => {
+      it('should get authors by id from opds', async () => {
+        const authorsResultList = await flibustaApi.getBooksByAuthorOpds(authorId);
+        
+        if (isNil(authorsResultList)) {
+          return;
+        }
+        
+        authorsResultList.forEach((authorBook) => {
+          expect(authorBook.author).to.satisfy(
+            (author: SearchBooksByNameOpdsResult['author']) => author.every(
+              (info) => isString(info.name) && isString(info.uri),
+            ),
+          );
+          expect(authorBook.title).to.satisfy((title: SearchBooksByNameOpdsResult['title']) => isString(title));
+          expect(authorBook.updated).to.satisfy(
+            (updated: SearchBooksByNameOpdsResult['updated']) => isString(updated),
+          );
+          expect(authorBook.categories).to.satisfy(
+            (categories: SearchBooksByNameOpdsResult['categories']) => categories.every(
+              (category) => isString(category),
+            ),
+          );
+          expect(authorBook.cover).to.satisfy(
+            (cover: SearchBooksByNameOpdsResult['cover']) => isNil(cover) || isString(cover),
+          );
+          expect(authorBook.downloads).to.satisfy(
+            (downloads: SearchBooksByNameOpdsResult['downloads']) => downloads.every(
+              (downloadsInfo) => isString(downloadsInfo.link) && isString(downloadsInfo.type),
+            ),
+          );
+          expect(authorBook.description).to.satisfy(
+            (description: SearchBooksByNameOpdsResult['description']) => isString(description),
+          );
+        });
+      });
+      
+      it('should return undefined authors by id from opds', async () => {
+        const authorsResultList = await flibustaApi.getBooksByAuthorOpds(Number.POSITIVE_INFINITY);
+        
+        expect(authorsResultList).to.be.equal(undefined);
+      });
+    });
+    
+    describe('getAuthorsFromOpdsPaginated()', async () => {
+      it('should get paginated authors by id from opds', async () => {
+        const authorsResultList = await flibustaApi.getBooksByAuthorOpdsPaginated(authorId);
+        
+        if (isNil(authorsResultList)) {
+          return;
+        }
+        
+        const {
+          items,
+          currentPage,
+          hasNextPage,
+          hasPreviousPage,
+        } = authorsResultList;
+        
+        items.forEach((authorBook) => {
+          expect(authorBook.author).to.satisfy(
+            (author: SearchBooksByNameOpdsResult['author']) => author.every(
+              (info) => isString(info.name) && isString(info.uri),
+            ),
+          );
+          expect(authorBook.title).to.satisfy((title: SearchBooksByNameOpdsResult['title']) => isString(title));
+          expect(authorBook.updated).to.satisfy(
+            (updated: SearchBooksByNameOpdsResult['updated']) => isString(updated),
+          );
+          expect(authorBook.categories).to.satisfy(
+            (categories: SearchBooksByNameOpdsResult['categories']) => categories.every(
+              (category) => isString(category),
+            ),
+          );
+          expect(authorBook.cover).to.satisfy(
+            (cover: SearchBooksByNameOpdsResult['cover']) => isNil(cover) || isString(cover),
+          );
+          expect(authorBook.downloads).to.satisfy(
+            (downloads: SearchBooksByNameOpdsResult['downloads']) => downloads.every(
+              (downloadsInfo) => isString(downloadsInfo.link) && isString(downloadsInfo.type),
+            ),
+          );
+          expect(authorBook.description).to.satisfy(
+            (description: SearchBooksByNameOpdsResult['description']) => isString(description),
+          );
+        });
+        
+        expect(currentPage).to.be.equal(0);
+        expect(hasNextPage).to.be.equal(true);
+        expect(hasPreviousPage).to.be.equal(false);
+      });
+      
+      it('should return undefined authors by id from opds', async () => {
+        const authorsResultList = await flibustaApi.getBooksByAuthorOpdsPaginated(Number.POSITIVE_INFINITY);
+        
+        expect(authorsResultList).to.be.equal(undefined);
+      });
+    });
+  });
 
   describe('getBooksByName', () => {
     const bookName = 'Этюд в багровых тонах; Знак четырех: Повести; Приключения Шерлока Холмса: Рассказы';
